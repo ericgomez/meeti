d96696b953ff622b8ddf37377bfaba17c0e96c20
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-11 16:10:42 -0600

    Configuring express-validator and Sequelize in Connect Flash

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require('express-validator')
 const User = require('../models/users')
 
 const formSignup = (req, res) => {
@@ -7,16 +8,34 @@ const formSignup = (req, res) => {
 }
 
 const signup = async (req, res) => {
+  const rules = [
+    body('confirm-password')
+      .notEmpty()
+      .withMessage('Confirm password is required'),
+    body('confirm-password')
+      .equals(req.body.password)
+      .withMessage('Confirm password must be the same as password')
+  ]
+
+  await Promise.all(rules.map(validation => validation.run(req)))
+
+  const errorsExp = validationResult(req)
+
   try {
     const user = await User.create(req.body)
 
     console.log(`User created: ${user}`)
   } catch (error) {
-    // get only the errors message
+    // get only the errors message from Sequelize
     const errorsSequelize = error.errors.map(err => err.message)
-    // console.log(errorsSequelize)
 
-    req.flash('error', errorsSequelize)
+    // get only the errors message from express-validator
+    const errorsExpress = errorsExp.errors.map(err => err.msg)
+
+    // merge both errors
+    const listErrors = [...errorsSequelize, ...errorsExpress]
+
+    req.flash('error', listErrors)
     res.redirect('/signup')
   }
 }
