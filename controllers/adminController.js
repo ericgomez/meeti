a87aea3a9c861bac033e6f85c2d6cb57213336ed
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-14 16:38:02 -0600

    Consulting groups and printing in HTML

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,11 @@
-const adminPanel = (req, res) => {
+const Group = require('../models/groups')
+
+const adminPanel = async (req, res) => {
+  const groups = await Group.findAll({ where: { userId: req.user.id } })
+
   res.render('admin/index', {
-    title: 'Admin Panel'
+    title: 'Admin Panel',
+    groups
   })
 }
 
