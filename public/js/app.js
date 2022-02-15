4a0629843ecf7add95b53ba40ba7f202c2bf905e
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-15 16:36:01 -0600

    Configuring Map and GeoCoding

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,34 @@
-var map = L.map('map').setView([51.505, -0.09], 13)
+import { OpenStreetMapProvider } from 'leaflet-geosearch'
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map)
+const lat = 15.861618
+const lng = -97.063522
 
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-  .openPopup()
+const map = L.map('map').setView([lat, lng], 13)
+
+document.addEventListener('DOMContentLoaded', () => {
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map)
+
+  const search = document.getElementById('searchForm')
+  search.addEventListener('input', searchDirection)
+})
+
+const searchDirection = e => {
+  // debounce search
+  clearTimeout(window.timer)
+  window.timer = setTimeout(() => {
+    const provider = new OpenStreetMapProvider()
+
+    provider
+      .search({ query: e.target.value || '' })
+      .then(results => {
+        const { x: lng, y: lat } = results[0]
+        map.setView([lat, lng], 13)
+      })
+      .catch(err => console.error(err))
+  }, 1000)
+}
+
+// debounce search
