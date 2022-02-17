a09d8149e509704d62895913d8183a54311fb467
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-17 16:59:34 -0600

    Grouping Meeti by past and upcoming

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize')
 const moment = require('moment')
 
 const Group = require('../models/groups')
@@ -5,7 +6,14 @@ const Meeti = require('../models/meeti')
 
 const adminPanel = async (req, res) => {
   const groupPromise = Group.findAll({ where: { userId: req.user.id } })
-  const meetiPromise = Meeti.findAll({ where: { userId: req.user.id } })
+  const meetiPromise = Meeti.findAll({
+    where: {
+      userId: req.user.id,
+      date: {
+        [Op.gte]: moment().format('YYYY-MM-DD')
+      }
+    }
+  })
 
   const [groups, meeti] = await Promise.all([groupPromise, meetiPromise])
 
