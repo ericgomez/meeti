416f9f3a2fb8a74cf9e3db2cfec56db8eae747ae
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-17 16:25:33 -0600

    Consulting the Meeti´s

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,11 +1,16 @@
 const Group = require('../models/groups')
+const Meeti = require('../models/meeti')
 
 const adminPanel = async (req, res) => {
-  const groups = await Group.findAll({ where: { userId: req.user.id } })
+  const groupPromise = Group.findAll({ where: { userId: req.user.id } })
+  const meetiPromise = Meeti.findAll({ where: { userId: req.user.id } })
+
+  const [groups, meeti] = await Promise.all([groupPromise, meetiPromise])
 
   res.render('admin/index', {
     title: 'Admin Panel',
-    groups
+    groups,
+    meeti
   })
 }
 
