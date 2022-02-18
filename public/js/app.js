e1ed993af6d3e85adbca98d16471a05fd6bd2c21
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-18 10:25:20 -0600

    Allowing the User to modify the location

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,15 @@
 import { OpenStreetMapProvider } from 'leaflet-geosearch'
 import * as Geocoding from 'esri-leaflet-geocoder'
 
-const lat = 15.861618
-const lng = -97.063522
+//Reverse geocoding - get address from lat-lng
+const geocodeService = Geocoding.geocodeService({
+  apikey:
+    'AAPKf2cd41a3a82f40a0aec0ca4dc2c37430CQIXi2F35seg8WeZsNc8Sc8vNvf9VDmMXSIBf5Psave4PVn2GLbVaKsIzl--jaL3'
+})
+
+const lat = document.getElementById('lat').value || 15.861618
+const lng = document.getElementById('lng').value || -97.063522
+const address = document.getElementById('address').value || ''
 
 const map = L.map('map').setView([lat, lng], 13)
 let markers = new L.FeatureGroup().addTo(map)
@@ -25,11 +32,6 @@ const searchDirection = e => {
     // delete previous markers
     markers.clearLayers()
 
-    //Reverse geocoding - get address from lat-lng
-    const geocodeService = Geocoding.geocodeService({
-      apikey:
-        'AAPKf2cd41a3a82f40a0aec0ca4dc2c37430CQIXi2F35seg8WeZsNc8Sc8vNvf9VDmMXSIBf5Psave4PVn2GLbVaKsIzl--jaL3'
-    })
     const provider = new OpenStreetMapProvider()
 
     provider
@@ -97,4 +99,43 @@ const getLocation = result => {
   document.getElementById('country').value = result.address.CountryCode || ''
   document.getElementById('lat').value = result.latlng.lat || ''
   document.getElementById('lng').value = result.latlng.lng || ''
+}
+
+if (lat && lng) {
+  markers.clearLayers()
+
+  marker = new L.marker([lat, lng], {
+    draggable: true, // make the marker draggable
+    autoPan: true // auto pan to the location
+  })
+    .addTo(map)
+    .bindPopup(address) // bind a popup to the marker
+    .openPopup() // open the popup
+
+  // add marker to group
+  markers.addLayer(marker)
+
+  // detect dragend event
+  marker.on('dragend', e => {
+    const {
+      _latlng: { lat, lng }
+    } = e.target
+    // update marker position
+    map.panTo([lat, lng])
+
+    // Reverse geocoding - get address from lat-lng
+    geocodeService
+      .reverse()
+      .latlng({ lat, lng })
+      .run((error, result) => {
+        if (error) {
+          console.log(error)
+        } else {
+          getLocation(result)
+
+          // update marker label
+          marker.setPopupContent(result.address.LongLabel)
+        }
+      })
+  })
 }
