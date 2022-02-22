3fb85b8ea960fdea45464acf9ea7c73f2ebdb178
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-22 16:29:21 -0600

    Ending the queries for the search engine

--- controllers/frontend/searchControllerFE.js
+++ controllers/frontend/searchControllerFE.js
@@ -8,7 +8,33 @@ const User = require('../../models/users')
 const searchResults = async (req, res) => {
   const { category, title, city, country } = req.query
 
-  const meetis = await Meeti.findAll({})
+  const query =
+    category === '' ? '' : `where: { categoryId: { [Op.eq]: ${category} } }`
+  console.log(query)
+
+  const meetis = await Meeti.findAll({
+    where: {
+      title: { [Op.iLike]: `%${title}%` }, // Op.iLike: case insensitive
+      city: { [Op.iLike]: `%${city}%` }, // Op.iLike: case insensitive
+      country: { [Op.iLike]: `%${country}%` } // Op.iLike: case insensitive
+    },
+    include: [
+      {
+        model: Group,
+        query
+      },
+      {
+        model: User,
+        attributes: ['id', 'name', 'image']
+      }
+    ]
+  })
+
+  res.render('frontend/search', {
+    title: 'Buscar',
+    meetis,
+    moment
+  })
 }
 
 module.exports = { searchResults }
