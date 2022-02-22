a82d0065c08621cada0a2652fd812b9a250f4041
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-22 14:09:48 -0600

    Allow the creator of the Meeti to delete the comments

--- controllers/frontend/commentsControllerFE.js
+++ controllers/frontend/commentsControllerFE.js
@@ -1,4 +1,5 @@
 const Comment = require('../../models/comments')
+const Meeti = require('../../models/meeti')
 
 const addComment = async (req, res, next) => {
   const { id } = req.params
@@ -29,8 +30,20 @@ const deleteComment = async (req, res, next) => {
       return next()
     }
 
+    // get creator of the meeti
+    const meeti = await Meeti.findOne({ where: { id: comment.meetiId } })
+
     // req.user.id is the id of the logged user by passport
+    console.log(comment.userId, req.user.id, meeti.userId, req.user.id)
+
+    // check if the logged user is the creator of the comment
     if (comment.userId !== req.user.id) {
+      res.status(401).send('You are not authorized to delete this comment')
+      return next()
+    }
+
+    //check if the logged user is the creator of the meeti
+    if (meeti.userId !== req.user.id) {
       res.status(401).send('You are not authorized to delete this comment')
       return next()
     }
