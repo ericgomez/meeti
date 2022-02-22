bd783628950c7ee0583a74358d89e3722ce5b7e9
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-22 09:50:46 -0600

    Creating Model and checking if the user is authenticated

---
+++ models/comments.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize')
+const { v4: uuid } = require('uuid')
+
+const db = require('../config/db')
+const User = require('./users')
+const Meeti = require('./meeti')
+
+const Comment = db.define(
+  'comment',
+  {
+    id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: () => uuid()
+    },
+    text: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Comment cannot be empty'
+        }
+      }
+    }
+  },
+  {
+    timestamps: false
+  }
+)
+
+Comment.belongsTo(User)
+Comment.belongsTo(Meeti)
+
+module.exports = Comment
